cae9aa1eb0baef005eac9440f2910db339694bf7
bondarika <bondarika@yandex.ru>
2025-06-04 22:22:28 +0300

    fix base url

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/v1';
+const API_BASE_URL = 'http://backend:8080/api/v1';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
